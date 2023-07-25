ea725326239c82cf276c7df9b4c8d5a3c303b758
Igor Vitor <igorvitor.iv@gmail.com>
2023-07-25 14:48:53 -0300

    init

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/albums", getAlbums)
+	router.GET("/albums/:id", getAlbumByID)
+	router.POST("/albums", postAlbums)
+
+	router.Run("localhost:8080")
+}
